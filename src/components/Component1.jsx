d0870198a710f942e8a100e7b235ceef6f2e9d4a
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-06-06 10:01:35 -0300

    update

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Component1 = () => {
     return (
         <>
-            <h2>Componente 1</h2>
+            <h2>Componente 1j</h2>
             <NavLink to='/'>
                 <button>Back</button>
             </NavLink>
