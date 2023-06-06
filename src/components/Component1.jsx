f45d3aac1eba5f103367621de00c848612dc0ec8
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-06-06 10:47:34 -0300

    cambios

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -4,7 +4,8 @@ import { NavLink } from "react-router-dom";
 const Component1 = () => {
     return (
         <>
-            <h2>Componente 1j</h2>
+            <h2>Componente J</h2>
+            <p>Este es mi componente para practicar git flow</p>
             <NavLink to='/'>
                 <button>Back</button>
             </NavLink>
