ff1fa744b50420c9b3fa6ebc3699601fb0b2c5ef
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-06-06 10:59:06 -0300

    Ojos

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -5,7 +5,7 @@ const Component1 = () => {
     return (
         <>
             <h2>Componente J</h2>
-            <p>Este es mi componente para pracicar git flow</p>
+            <p>Este es mi componente para pracicar git flow 👀 </p>
             <NavLink to='/'>
                 <button>Back</button>
             </NavLink>
